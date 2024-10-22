8e9ee69f1a62fc7f75daf11486afb1f05e07ef04
taze-zarla <taze@zarla.com>
2024-10-22 22:02:57 +0800

    prepare favicon pack

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,68 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+
+const brandColor = "#0989d8";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+
+        {/* General Browsers */}
+        <meta name="theme-color" content={brandColor} />
+        <meta name="application-name" content="Sample" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/sample-icon-48x48.png"
+          sizes="48x48"
+        />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/sample-icon.svg"
+          sizes="any"
+        />
+        <link
+          rel="icon"
+          href="/sample-icon-48x48.ico"
+          sizes="48x48"
+        />
+        <link
+          rel="shortcut icon"
+          href="/sample-icon-48x48.ico"
+        />
+
+        {/* Edge and IE 12 */}
+        <meta
+          name="msapplication-TileImage"
+          content="/sample-mstile-150x150.png"
+        />
+        <meta name="msapplication-TileColor" content={brandColor} />
+        <meta
+          name="msapplication-config"
+          content="/msbrowserconfig.xml"
+        />
+
+        {/* Apple */}
+        <meta name="apple-mobile-web-app-title" content="Sample" />
+        <link
+          rel="apple-touch-icon"
+          href="/sample-apple-touch-icon-180x180.png"
+        />
+        <link
+          rel="mask-icon"
+          href="/sample-icon.svg"
+          color="#0989d8"
+        />
+
+        {/* Android Chrome */}
+        <link
+          rel="manifest"
+          href="/site.webmanifest"
+        />
       </Head>
 
       <main>
@@ -19,5 +74,5 @@ export default function Home() {
 
       <Footer />
     </div>
-  )
+  );
 }
