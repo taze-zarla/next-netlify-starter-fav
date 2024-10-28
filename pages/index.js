0137a1affe21c453e6e60eb740f92b9ab217eae2
taze-zarla <taze@zarla.com>
2024-10-28 23:14:20 +0800

    attempt transparent z for favicon

--- pages/index.js
+++ pages/index.js
@@ -51,45 +51,11 @@ export default function Home() {
           content="https://opengraph.b-cdn.net/production/images/d225befc-acee-4f32-98ca-0968657d9cdf.png?token=ZQW9XstBS1NNyuDjfosdiLEIxZlP0uRKVle1G18fJww&height=630&width=1200&expires=33265610773"
         />
 
-        {/* General Browsers */}
-        <meta name="theme-color" content={brandColor} />
-        <meta name="application-name" content="Sample" />
-
-        {/* prefer 48x48 png */}
-        <link
-          rel="icon"
-          type="image/png"
-          href="/sample-icon-48x48.png"
-          sizes="48x48"
-        />
-        <link
-          rel="icon"
-          type="image/svg+xml"
-          href="/sample-icon.svg"
-          sizes="any"
-        />
-
-        {/* .ico only for last resort fallback for non-modern devices */}
-        <link rel="icon" href="/sample-icon.ico" sizes="16x16 32x32 48x48" />
-        <link rel="shortcut icon" href="/sample-icon.ico" />
-
-        {/* Edge and IE 12 */}
-        <meta
-          name="msapplication-TileImage"
-          content="/sample-mstile-150x150.png"
-        />
-        <meta name="msapplication-TileColor" content={brandColor} />
-        <meta name="msapplication-config" content="/msbrowserconfig.xml" />
-
-        {/* Apple */}
-        <meta name="apple-mobile-web-app-title" content="Sample" />
-        <link
-          rel="apple-touch-icon"
-          href="/sample-apple-touch-icon-180x180.png"
-        />
-
-        {/* Android Chrome */}
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main>
